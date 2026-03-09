3bdde520db85033767a55fc12e4fba18ed3f9f15
Madhusekhar <madhusekhar852@gmail.com>
2026-03-09 22:25:04 +0530

    Connect frontend to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = 'http://127.0.0.1:9090'; 
+const API_BASE_URL = 'https://farmate-backend-kjnu.onrender.com'; 
 
 export const getCropRecommendation = async (formData) => {
   try {
